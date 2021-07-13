f89c07bb707f34a1d24225d5cb4b48534d00ec1e
Maxim Tishin <Maxim Tishin@DISTISHIN>
2021-07-13 20:32:15 +0300

    Все в одном классе

--- IdeaProjects/Calculator/src/calculator/Calculator.java
+++ IdeaProjects/Calculator/src/calculator/Calculator.java
@@ -3,7 +3,32 @@ package calculator;
 public class Calculator {
     
     public static void main(String[] args) {
-       
+       int a = 6;
+       int b = 2;
+       sum(a, b);
+       multiple(a, b);
+       subtraction(a, b);
+       division(a, b);
     }
     
+   static void sum(int a, int b) {       
+       int c = a + b;
+       System.out.println(c);       
+   }
+   
+   static void multiple(int a, int b) {
+       int c = a * b;
+       System.out.println(c);
+   }
+   
+   static void subtraction(int a, int b) {
+       int c = a - b;
+       System.out.println(c);
+   }
+   
+   static void division(int a, int b) {
+       int c = a / b;
+       System.out.println(c);
+   }
+   
 }
