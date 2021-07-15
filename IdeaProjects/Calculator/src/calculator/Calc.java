d7c6d3dc40f627ac593857b2eeb59cc7e5b50cdf
Maxim Tishin <Maxim Tishin@DISTISHIN>
2021-07-15 18:05:45 +0300

    Готовое решение

---
+++ IdeaProjects/Calculator/src/calculator/Calc.java
@@ -0,0 +1,26 @@
+package calculator;
+
+public class Calc {
+    public static int calculated(int num1, int num2, char op) {
+//      Создаем переменную и присваиваем 0     
+        int result = 0;
+//      В зависимости от знака получаем результат     
+        switch(op) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            default:
+                throw new IllegalArgumentException("Не верный знак");
+        }
+        return result;
+}
+}
--- IdeaProjects/Calculator/src/calculator/Calculator.java
+++ IdeaProjects/Calculator/src/calculator/Calculator.java
@@ -1,34 +1,67 @@
 package calculator;
 
+// Добавляем библиотеки
+import java.util.Scanner;
+// Создаем общий класс Calculator
 public class Calculator {
     
-    public static void main(String[] args) {
-       int a = 6;
-       int b = 2;
-       sum(a, b);
-       multiple(a, b);
-       subtraction(a, b);
-       division(a, b);
+//   Создаем переменные и переменнуя scanner для ввода с клавиатуры
+     static Scanner scanner = new Scanner(System.in);    
+     static int num1, num2, result;
+     static char operation;
+        
+     public static void main(String[] args) {       
+        System.out.println("Введите выражение");
+//      Считываем строку input которую ввёл пользователь
+        String input = scanner.nextLine();        
+//      Создаем массив для хранеия символов: char_simv
+        char char_simv[];
+        char_simv = new char[10];
+//      Заполняем массив символами, который мог ввести пользователь
+        for(int i = 0; i < input.length(); i++) {
+//      Записываем метод извлечения символа из строки            
+            char_simv[i] = input.charAt(i);
+            if(char_simv[i] == '+')
+                operation = '+';
+            if(char_simv[i] == '-')
+                operation = '-'; 
+            if(char_simv[i] == '*') {
+                operation = '*';
+            }
+            if(char_simv[i] == '/') {
+                operation = '/';
+            }
+        }
+//      Преобразуем строку в числовой тип данных
+        String under_charString = String.valueOf(char_simv);
+//      Напишем метод split для разделителей +-*/
+        String[] separator = under_charString.split("[+-/*]");
+        String stable00 = separator[0];
+        String stable01 = separator[1];
+//      Метод trim удаляет начальные и конечные пробелы
+        String string03 = stable01.trim();             
+//      Присваеваем переменным num1 и num2 ссылку на метод для ввода римских цифр
+        num1 = Roman.romanNumber(stable00);
+        num2 = Roman.romanNumber(string03);
+        if(num1 < 0 && num2 < 0){
+            result = 0;            
+        } else {
+            result = Calc.calculated(num1, num2, operation);
+            System.out.println("Римские цифры");
+            String resultRoman = ConvertToRoman.convertToRoman(result);
+            System.out.println(stable00 + " " + operation + " " + string03 + " = " + resultRoman);
+        }
+//      Преобразуем строку в число с использование parseInt класса Integer
+        num1 = Integer.parseInt(stable00);
+        num2 = Integer.parseInt(string03);        
+//      Получаем результат
+        result = Calc.calculated(num1, num2, operation);
+        System.out.println("Арабские цифры");
+        System.out.println(num1 + " " + operation + " " + num2 + " = " + result);
+                
     }
-    
-   static void sum(int a, int b) {       
-       int c = a + b;
-       System.out.println(c);       
-   }
-   
-   static void multiple(int a, int b) {
-       int c = a * b;
-       System.out.println(c);
-   }
-   
-   static void subtraction(int a, int b) {
-       int c = a - b;
-       System.out.println(c);
-   }
-   
-   static void division(int a, int b) {
-       int c = a / b;
-       System.out.println(c);
-   }
-   
 }
+    
+
+   
+
