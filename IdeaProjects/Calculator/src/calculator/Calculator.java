599a3c52beb77b11af16d4e537c72abdd8c3e1fe
Maxim Tishin <Maxim Tishin@DISTISHIN>
2021-07-17 15:49:17 +0300

    Исходные файлы

--- IdeaProjects/Calculator/src/calculator/Calculator.java
+++ IdeaProjects/Calculator/src/calculator/Calculator.java
@@ -3,8 +3,7 @@ package calculator;
 // Добавляем библиотеки
 import java.util.Scanner;
 // Создаем общий класс Calculator
-public class Calculator {
-    
+public class Calculator {    
 //      Создаем переменные и переменнуя scanner для ввода с клавиатуры
     static Scanner scanner = new Scanner(System.in);    
     static int num1, num2, result;
@@ -43,31 +42,31 @@ public class Calculator {
 //      Присваеваем переменным num1 и num2 ссылку на метод для ввода римских цифр
         num1 = Roman.romanNumber(stable00);
         num2 = Roman.romanNumber(string03);
-        if(num1 < 1 || num2 < 1 || num1 > 10 || num2 > 10) {
- //           result = 0;
-        System.out.println("Ошибка. Введите числа от 1 до 10");
+        if(num1 < 1 || num2 < 1) {
+            result = 0;  
         } else {
+//      Получаем результат
             result = Calc.calculated(num1, num2, operation);
             System.out.println("Римские цифры");
             String resultRoman = ConvertToRoman.convertToRoman(result);
             System.out.println(stable00 + " " + operation + " " + string03 + " = " + resultRoman);            
- //       }
+        }
         try {
 //      Преобразуем строку в число с использование parseInt класса Integer
             num1 = Integer.parseInt(stable00);
             num2 = Integer.parseInt(string03); 
 //      Если числа меньше 1 или больше 10 то выводится сообщение.        
-  //       if(num1 < 1 || num2 < 1 || num1 > 10 || num2 > 10) {
-  //          System.out.println("Ошибка. Введите числа от 1 до 10");
-  //      }
-  //      else {
+         if(num1 < 1 || num2 < 1 || num1 > 10 || num2 > 10) {
+            System.out.println("Ошибка. Введите числа от 1 до 10");
+        }
+        else {
 //      Получаем результат
             result = Calc.calculated(num1, num2, operation);
             System.out.println("Арабские цифры");
             System.out.println(num1 + " " + operation + " " + num2 + " = " + result);
-  //      }
+        }
             } catch(NumberFormatException e) {}
-        }       
+            
     }
 }
     
